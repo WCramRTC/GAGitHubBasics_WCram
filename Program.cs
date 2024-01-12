634ebec53fba22bd04701466d1ac9f2ac01cb35d
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-01-12 12:08:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace GAGitHubBasics_WCram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // Start of Main Code Block
+            // Code goes inside of here
+
+            Console.WriteLine("Hello, Will!");
+
+            // Console keyword that access actions with the Console
+            // The period, ., gives us access to the commands
+            // We type the command followed by parentheses
+            // semi color, ;, signifies the end of our action.
+            // Console . WriteLine()
+            Console.WriteLine("I got my BAS in Application Development");
+
+        } // End of Main Code Block
+    }
+}
